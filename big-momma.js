9c13e2ec03de8ee8d6b65983fa559f0f91127e94
jaymark <jaykmark@gmail.com>
2020-06-08 19:23:13 -0700

    Refactor code.

--- big-momma.js
+++ big-momma.js
@@ -4,13 +4,17 @@ const axios = require('axios');
 const cryptoKittiesAddress = '0x06012c8cf97BEaD5deAe237070F9587f8E7A266d';
 const cryptoKittiesAbiUrl = 'https://api.etherscan.io/api?module=contract&action=getabi&address=0x06012c8cf97bead5deae237070f9587f8e7a266d';
 
-const queryCryptoKitties = async () => {
+const main = async (startingBlock, endingBlock) => {
   try {
     const cryptoKittiesAbi = await fetchContractAbi(cryptoKittiesAbiUrl);
-    const contract = new web3.eth.Contract(cryptoKittiesAbi, cryptoKittiesAddress);
-    const bigMommaId = await findBigMomma(contract, 6607985, 7028323)
-    // getKittyDetails(contract, bigMommaId);
-    // getKittyDetails(contract, 1);
+    const cryptoKittiesContract = new web3.eth.Contract(cryptoKittiesAbi, cryptoKittiesAddress);
+    const cryptoKittyBirthEvents = await queryPastEventsByType(cryptoKittiesContract, 'Birth', startingBlock, endingBlock);
+    console.log(`Total Births: ${cryptoKittyBirthEvents.length}`);
+    const cryptoKittyBirthFreqMap = countKittyBirthsFreqById(cryptoKittyBirthEvents);
+    const bigMommaId = convertFreqMapToArrayAndFindBigMommaId(cryptoKittyBirthFreqMap);
+    const bigMommaDetails = await getKittyDetailsById(cryptoKittiesContract, bigMommaId);
+    console.log("bigMommaDetails: ", bigMommaDetails);
+    return bigMommaDetails;
   } catch (err) {
     console.error(err);
   }
@@ -26,90 +30,65 @@ const fetchContractAbi = async (url) => {
   }
 };
 
-const findBigMomma = async (contract, startingBlock, endingBlock) => {
-  try {
-    const totalBlocks = endingBlock - startingBlock;
-    const batchesOfFiveThousand = Math.floor(totalBlocks / 5000);
-    const batchedBlocks = batchesOfFiveThousand * 5000;
-    const remainingBlocksAfterBatches = totalBlocks - batchedBlocks;
-    const allKittyMommaBirths = new Map();
-    let totalKittyBirths = 0;
+// Query in batches of max 5000 blocks.
+const queryPastEventsByType = async (contract, eventType, startingBlock, endingBlock) => {
+  console.log(`Start Block: ${startingBlock} to End Block: ${endingBlock}`);
+  const promises = [];
+  let cursor = startingBlock;
 
-    console.log(`Start Block: ${startingBlock} to End Block: ${endingBlock} => ${totalBlocks} Total Blocks`);
-
-    console.log(`Querying blocks ${startingBlock} -> ${startingBlock + batchedBlocks - 1} in batches of 5000...`);
-    // Query in batches of 5,000 blocks.
-    for (let i = 0; i < batchedBlocks - 5000; i += 5000) {
-      const kittyBirths = await contract.getPastEvents('Birth', {
-        fromBlock: startingBlock + i,
-        toBlock: startingBlock + i + 4999,
-      })
-      console.log(`Querying ${startingBlock + i} -> ${startingBlock + i + 4999}`)
-      totalKittyBirths += countKittyBirthsFreq(kittyBirths, allKittyMommaBirths);
-    }
-
-    // Query remaining blocks.
-    console.log(`Querying blocks ${startingBlock + batchedBlocks} -> ${startingBlock + batchedBlocks + remainingBlocksAfterBatches}`)
-
-    const kittyBirths = await contract.getPastEvents('Birth', {
-      fromBlock: startingBlock + batchedBlocks,
-      toBlock: startingBlock + batchedBlocks + remainingBlocksAfterBatches,
-    })
-    totalKittyBirths += countKittyBirthsFreq(kittyBirths, allKittyMommaBirths);
-
-    console.log(`Total births: ${totalKittyBirths}`);
-
-    let bigMommaId;
-    const allKittyMommas = [];
-    for (let [kittyId, births] of Object.entries(allKittyMommaBirths)) {
-      // if (!bigMommaId || births > allKittyMommaBirths[bigMommaId]) {
-      //   bigMommaId = kittyId;
-      // }
-      allKittyMommas.push([kittyId, births])
-    }
-    allKittyMommas.sort((a, b) => b[1] - a[1]);
-    console.log(`
-      Top 5 Mommas:
-      1) ID: ${allKittyMommas[0][0]}, Births: ${allKittyMommas[0][1]}
-      2) ID: ${allKittyMommas[1][0]}, Births: ${allKittyMommas[1][1]}
-      3) ID: ${allKittyMommas[2][0]}, Births: ${allKittyMommas[2][1]}
-      4) ID: ${allKittyMommas[3][0]}, Births: ${allKittyMommas[3][1]}
-      5) ID: ${allKittyMommas[4][0]}, Births: ${allKittyMommas[4][1]}
-    `)
-    // return bigMommaId;
-  } catch (err) {
-    console.error(err);
+  while (cursor < endingBlock) {
+    batchEnd = cursor + 5000 < endingBlock ? cursor + 4999 : endingBlock;
+    promises.push(contract.getPastEvents(eventType, {
+      fromBlock: cursor,
+      toBlock: batchEnd,
+    }));
+    cursor += 5000;
   }
+  const resolvedPromises = await Promise.all(promises);
+  return resolvedPromises.reduce((acc, currentValue) => acc.concat(...currentValue), []);
 };
 
-const countKittyBirthsFreq = (kittyBirths, allKittyMommaBirths) => {
-  let totalKittyBirths = 0;
+const countKittyBirthsFreqById = kittyBirthEvents => {
+  let countKittyBirths = 0;
+  const kittyBirthEventsMap = {};
 
-  kittyBirths.forEach((birth) => {
-    totalKittyBirths += 1;
-    if (birth.returnValues.matronId != '0') {
-      if (allKittyMommaBirths[birth.returnValues.matronId]) {
-        allKittyMommaBirths[birth.returnValues.matronId] += 1;
+  kittyBirthEvents.forEach(birth => {
+    countKittyBirths += 1;
+    if (birth.returnValues.matronId !== '0') {
+      // Store frequency of births by kittyID in map
+      if (kittyBirthEventsMap[birth.returnValues.matronId]) {
+        kittyBirthEventsMap[birth.returnValues.matronId] += 1;
       } else {
-        allKittyMommaBirths[birth.returnValues.matronId] = 1;
+        kittyBirthEventsMap[birth.returnValues.matronId] = 1;
       }
     }
   });
-  return totalKittyBirths;
+
+  return kittyBirthEventsMap;
 };
 
-const getKittyDetails = (contract, kittyId) => {
-  contract.methods.getKitty(kittyId).call((err, res) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(`
-      Kitty's birth timestamp: ${res.birthTime} => ${new Date(res.birthTime * 1000)}
-      Kitty's generation: ${res.generation}
-      Kitty's genes: ${res.genes}
-    `);
-  });
+const convertFreqMapToArrayAndFindBigMommaId = (kittyBirthFreqMap) => {
+  const kittyBirthEventsArray = [];
+
+  for (let [kittyId, births] of Object.entries(kittyBirthFreqMap)) {
+    kittyBirthEventsArray.push([kittyId, births]);
+  }
+
+  kittyBirthEventsArray.sort((a, b) => b[1] - a[1]);
+
+  console.log(`
+    Top 3 Mommas:
+    1) ID: ${kittyBirthEventsArray[0][0]}, Births: ${kittyBirthEventsArray[0][1]}
+    2) ID: ${kittyBirthEventsArray[1][0]}, Births: ${kittyBirthEventsArray[1][1]}
+    3) ID: ${kittyBirthEventsArray[2][0]}, Births: ${kittyBirthEventsArray[2][1]}`);
+
+  const bigMommaId = kittyBirthEventsArray[0][0];
+  return bigMommaId;
 };
 
-queryCryptoKitties();
+const getKittyDetailsById = async (contract, kittyId) => {
+  const res = await contract.methods.getKitty(kittyId).call();
+  return res;
+};
+
+main(6607985, 7028323);
